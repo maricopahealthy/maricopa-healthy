92eeb5a9a3511cf82055d4d72fe7672b475253e5
Oscar Sandoval <apollo.sandoval@gmail.com>
2019-04-14 13:41:01 -0700

    added SectionList and SectionUtility

--- client/mobile/screens/Recipes/RecipesRecordScreen.js
+++ client/mobile/screens/Recipes/RecipesRecordScreen.js
@@ -1,12 +1,47 @@
 // todo: RecipesRecordScreen
 import React from 'react';
-import { StyleSheet, FlatList } from 'react-native';
+import { StyleSheet, SectionList } from 'react-native';
 import { Container, Content, Card, CardItem, Left, Thumbnail, Body, Text, Right, Icon, ListItem } from "native-base";
+import Section from '../../utils/SectionsUtility';
+
+// sections to display for Recipe Record
+const sections = [
+  Section('Ingredients', [{id: 0, title: 'text'}], ({item}) => {
+    return (<Text>Is this thing on?</Text>);
+  }),
+  Section('Directions', [], ({item}) => {
+    return (<Text>Is this thing on?</Text>);
+  }),
+  Section('Nutrition Facts (Per Serving)', [], ({item}) => {
+    return (<Text>Is this thing on?</Text>);
+  }),
+  Section('Reviews', [], ({item}) => {
+    return (<Text>Is this thing on?</Text>);
+  })
+];
 
 /**
- *
+ * returns a unique id property for generating the necessary 'key' of a react list.
+ * @param id
+ * @returns {*}
+ */
+const extractKey = ({id}) => id;
+
+/**
+ * RecipesRecord displays a detailed Screen for a single recipe from the RecipeList.
  */
 export default class RecipesRecordScreen extends React.Component {
+
+  /**
+   * Renders the section title and adds header specific styling.
+   */
+  renderSectionHeader =({section}) => {
+    return (
+        <ListItem itemDivider>
+          <Text>{section.title}</Text>
+        </ListItem>
+    )
+  }
 
   render() {
     return (
@@ -17,7 +52,7 @@ export default class RecipesRecordScreen extends React.Component {
                 <Left>
                   <Thumbnail />
                   <Body>
-                    <Text>This is the card title</Text>
+                    <Text>Apple Pecan Bars</Text>
                   </Body>
                 </Left>
                 <Right>
@@ -25,20 +60,12 @@ export default class RecipesRecordScreen extends React.Component {
                 </Right>
               </CardItem>
             </Card>
-            <FlatList>
-              <ListItem divider>
-                <Text>Ingredients</Text>
-              </ListItem>
-              <ListItem divider>
-                <Text>Directions</Text>
-              </ListItem>
-              <ListItem divider>
-                <Text>Nutrition Facts (Per Serving)</Text>
-              </ListItem>
-              <ListItem divider>
-                <Text>Reviews</Text>
-              </ListItem>
-            </FlatList>
+          {/* Section information */}
+          <SectionList
+              sections={sections}
+              renderSectionHeader={this.renderSectionHeader}
+              keyExtractor={extractKey}
+          />
           </Content>
         </Container>
     )
