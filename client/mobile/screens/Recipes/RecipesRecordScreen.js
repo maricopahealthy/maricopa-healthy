d1ece64572b9a0a8545b57f48c5a01795c99f91d
Oscar Sandoval <apollo.sandoval@gmail.com>
2019-04-14 17:03:47 -0700

    added direction section to RecipesRecord screen

--- client/mobile/screens/Recipes/RecipesRecordScreen.js
+++ client/mobile/screens/Recipes/RecipesRecordScreen.js
@@ -4,13 +4,62 @@ import { StyleSheet, SectionList } from 'react-native';
 import { Container, Content, Card, CardItem, Left, Thumbnail, Body, Text, Right, Icon, ListItem } from "native-base";
 import Section from '../../utils/SectionsUtility';
 
+// Used for sample data ==================================
+const ingredientData = [
+  {
+    id: 0,
+    instruction: '4 cups shredded apples with peel'
+  },
+  {
+    id: 1,
+    instruction: '2 2/3 cup quick rolled oats',
+  },
+  {
+    id: 2,
+    instruction: '2/3 cup finely chopped pecans',
+  },
+  {
+    id: 3,
+    instruction: '2 eggs, beaten',
+  }
+];
+
+const directionsData = [
+  {
+    id: 0,
+    direction: 'Wash hands with soap and warm water. Wash fresh produce before preparing.',
+  },
+  {
+    id: 1,
+    direction: 'Preheat oven to 375 degrees. Spray 9x9 pan with non-stick spray. Set aside.',
+  },
+  {
+    id: 2,
+    direction: 'Shred apples on a large-hole grater and place in a large mixing bowl. Discard core and seeds.',
+  },
+  {
+    id: 3,
+    direction: 'Combine oats, pecans, brown sugar, cinnamon, baking powder, baking soda, and salt in a large mixing bowl. Mix well.',
+  },
+]
+
+// remove above =============================================
+
 // sections to display for Recipe Record
 const sections = [
-  Section('Ingredients', [{id: 0, title: 'text'}], ({item}) => {
-    return (<Text>Is this thing on?</Text>);
+  Section('Ingredients', ingredientData, ({item}) => {
+    return (
+        <ListItem>
+          <Text>{item.instruction}</Text>
+        </ListItem>
+    );
   }),
-  Section('Directions', [], ({item}) => {
-    return (<Text>Is this thing on?</Text>);
+  Section('Directions', directionsData, ({item, index}) => {
+    return (
+        <ListItem>
+          <Text>{index + 1}. {item.direction}</Text>
+        </ListItem>
+    );
   }),
   Section('Nutrition Facts (Per Serving)', [], ({item}) => {
     return (<Text>Is this thing on?</Text>);
