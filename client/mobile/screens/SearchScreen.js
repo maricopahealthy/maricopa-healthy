8980e5273e815047988290feec660af8c527fc5c
Marcus Ames <marcusames00@gmail.com>
2019-05-07 12:13:35 -0700

    Added search screen navigation header

--- client/mobile/screens/SearchScreen.js
+++ client/mobile/screens/SearchScreen.js
@@ -2,16 +2,14 @@
 // note: SearchResultsList component
 import React from 'react';
 import { View, FlatList } from 'react-native';
-import { Container, Header, Item, Input, Icon, Button } from 'native-base';
+import { Container } from 'native-base';
 import RecordTile from '../components/RecordTile';
 
 const results = [
-  { key: 'Market1', record: 'Market 1' },
-  { key: 'Market2', record: 'Market 2' },
-  { key: 'Park1', record: 'Park 1' },
-  { key: 'Recipe1', record: 'Recipe 1' },
-  { key: 'Activity1', record: 'Activity 1' },
-  { key: 'Activity2', record: 'Activity 2' }
+  { key: 'Market1', title: 'Avondale Care1st Resource', address: '16838 E Ave of the Fountains Fountain Hills, AZ 85268', image: require("../assets/thumbnails/markets/markets-thumb-placeholder-01.png") },
+  { key: 'Market2', title: 'Our Harvest/Nuestra Cosec', address: '3806 N. Brown Rd Scottsdale, AZ 85251', image: require("../assets/thumbnails/markets/markets-thumb-placeholder-02.png") },
+  { key: 'Market3', title: 'Anthem Farmers Market', address: '41703 N. Gavilan Peak Pkwy Anthem, AZ 85086', image: require("../assets/thumbnails/markets/markets-thumb-placeholder-03.png") },
+  { key: 'Market4', title: 'Market Names Goes Here', address: '5757 N. Central Ave Phoenix, AZ 85013', image: require("../assets/thumbnails/markets/markets-thumb-placeholder-04.png") },
 ]
 
 /**
@@ -22,15 +20,6 @@ export default class SearchScreen extends React.Component {
   render() {
     return (
       <Container>
-        <Header searchBar rounded>
-          <Item>
-            <Icon name="ios-search" />
-            <Input placeholder="Search" />
-          </Item>
-          <Button transparent>
-            <Icon name="ios-close" />
-          </Button>
-        </Header>
         <View>
           {/* //todo Make search results dynamic */}
           {/* //todo Make search results bring you to individual record */}
