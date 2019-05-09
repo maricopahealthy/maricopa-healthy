976922ff957629759dc102a14b6387c4963ae7b5
Marcus Ames <marcusames00@gmail.com>
2019-05-09 12:49:53 -0700

    added clear functionality to search

--- client/mobile/components/navigation-headers/NavigationalHeaderSearch.js
+++ client/mobile/components/navigation-headers/NavigationalHeaderSearch.js
@@ -10,24 +10,29 @@ import { Button, Header, Icon, Item, Input, Text } from "native-base";
  * @param {string} title - the name of the resource to display in the center of the navbar.
  * @returns - a functional component wrapped by Higher-order component: withNavigation().
  */
-const NavigationalHeaderSearch = ({ navigation }) => {
-  return (
-    <Header searchBar rounded style={styles.color}>
-      <Item style={{backgroundColor: "white"}}>
-        <Icon name="ios-search" />
-        <Input placeholder="Search" />
-        <Button style={{ backgroundColor: "#8E8E93", height: "80%", marginTop: 4, marginRight: 8}}>
-          <Text style={{ color: "white", fontSize: 10 }}>CLEAR</Text>
-        </Button>
-      </Item>
-      <Button onPress={() => navigation.navigate("Home")} transparent>
-        <Icon style={{color: "white", fontSize: 40}} name="ios-close" />
-      </Button>
-    </Header>
-  )
-};
+export default class NavigationalHeaderSearch extends React.Component {
 
-export default withNavigation(NavigationalHeaderSearch);
+  state = {
+    search: ""
+  }
+
+  render() {
+    return (
+      <Header searchBar rounded style={styles.color}>
+        <Item style={{backgroundColor: "white"}}>
+          <Icon name="ios-search" />
+          <Input placeholder="Search" value={this.state.search} onChange={({target}) => this.setState({search: target.value})} />
+          <Button onPress={() => this.setState({search: ""})} style={{ backgroundColor: "#8E8E93", height: "80%", marginTop: 4, marginRight: 8}}>
+            <Text style={{ color: "white", fontSize: 10 }}>CLEAR</Text>
+          </Button>
+        </Item>
+        <Button onPress={() => this.props.navigation.navigate("Home")} transparent>
+          <Icon style={{color: "white", fontSize: 40}} name="ios-close" />
+        </Button>
+      </Header>
+    )
+  }
+};
 
 const styles = StyleSheet.create({
   color: {
