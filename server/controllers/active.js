8bfea24a65915037ee9892fe8b042675ff129468
Oscar Sandoval <apollo.sandoval@gmail.com>
2019-05-15 19:20:38 -0700

    renamed activities.js to active.js

---
+++ server/controllers/active.js
@@ -0,0 +1,42 @@
+const knex = require("../db/knex");
+
+module.exports = {
+  /**
+   * find returns all activities in Activities table.
+   * @param {Object} req - Express request object.
+   * @param {Object} res - Express response object.
+   * @return {Array<Activity>}
+   */
+  find: (req, res) => {
+    knex("active")
+      .then(data => res.send(data))
+      .catch(err => console.error(err));
+  },
+
+  /**
+   * findById() returns activity from Activities table matching unique _id property.
+   * @param {Object} req - Express request object.
+   * @param {Object} res - Express response object.
+   * @return {Activity}
+   */
+  findById: (req, res) => {
+    knex("active").where("id", req.params.id)
+      .then(data => res.send(data))
+      .catch(err => console.error(err));
+  },
+
+  /**
+   * findByIdAndDelete deletes activity from Activities table matching unique _id property and returns deleted item.
+   * @param req
+   * @param res
+   * @return {Activity}
+   */
+  findByIdAndDelete: (req, res) => {
+    knex("active")
+      .where("id", req.params.id)
+      .del("*")
+        .then(data => res.send(data))
+        .catch(err => console.error(err));
+  },
+
+};
