66da93b752cb97f4c5d374122b06735611acece4
Marcus Ames <marcusames00@gmail.com>
2019-05-18 12:42:32 -0700

    made records join table

--- server/db/seeds/08_favorites.js
+++ server/db/seeds/08_favorites.js
@@ -5,7 +5,14 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('favorites').insert([
-        {}
+        {
+          user_id: 1,
+          record_id: 1
+        },
+        {
+          user_id: 1,
+          record_id: 2
+        },
       ]);
     });
 };
