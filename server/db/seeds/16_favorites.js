376d1b5eb6a8f2dcce3fd3ebbb970bf5dbb45542
Oscar Sandoval <apollo.sandoval@gmail.com>
2019-05-21 18:59:51 -0700

    moved favorites data over from marcus branch

--- server/db/seeds/16_favorites.js
+++ server/db/seeds/16_favorites.js
@@ -1,18 +1,20 @@
-
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('favorites').del()
-    .then(function () {
+  return knex("favorites")
+    .del()
+    .then(function() {
       // Inserts seed entries
-      return knex('favorites').insert([
+      return knex("favorites").insert([
         {
           user_id: 1,
-          record_id: 1
+          record_type: "market",
+          market_id: 1
         },
         {
           user_id: 1,
-          record_id: 2
-        },
+          record_type: "park",
+          park_id: 2
+        }
       ]);
     });
 };
