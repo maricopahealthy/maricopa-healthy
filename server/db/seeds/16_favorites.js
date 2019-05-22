bd63f5551d85c207231707c255e469a7137c5baf
Oscar Sandoval <apollo.sandoval@gmail.com>
2019-05-21 19:41:48 -0700

    copy/paste favorites table from marcus branch

--- server/db/seeds/16_favorites.js
+++ server/db/seeds/16_favorites.js
@@ -1,18 +1,19 @@
-
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex('favorites').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('favorites').insert([
-        {
-          user_id: 1,
-          record_id: 1
-        },
-        {
-          user_id: 1,
-          record_id: 2
-        },
-      ]);
-    });
+      .then(function () {
+        // Inserts seed entries
+        return knex('favorites').insert([
+          {
+            user_id: 1,
+            record_type: "market",
+            market_id: 1
+          },
+          {
+            user_id: 1,
+            record_type: "park",
+            park_id: 2
+          },
+        ]);
+      });
 };
