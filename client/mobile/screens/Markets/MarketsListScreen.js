92b184670f9e7243337adffbbaadb3c177053001
Oscar Sandoval <apollo.sandoval@gmail.com>
2019-05-21 20:27:09 -0700

    formatting

--- client/mobile/screens/Markets/MarketsListScreen.js
+++ client/mobile/screens/Markets/MarketsListScreen.js
@@ -1,6 +1,6 @@
 // todo: MarketsListScreen
 import React from 'react';
-import { View, StyleSheet, FlatList } from 'react-native';
+import {View, StyleSheet, FlatList} from 'react-native';
 import RecordTile from '../../components/RecordTile'
 
 /**
@@ -10,17 +10,37 @@ export default class MarketsListScreen extends React.Component {
 
   render() {
     return (
-      <View>
-        <FlatList
-          data={[
-            { key: 'Market1', title: 'Avondale Care1st Resource', address: '16838 E Ave of the Fountains Fountain Hills, AZ 85268', image: require("../../assets/thumbnails/markets/markets-thumb-placeholder-01.png") },
-            { key: 'Market2', title: 'Our Harvest/Nuestra Cosec', address: '3806 N. Brown Rd Scottsdale, AZ 85251', image: require("../../assets/thumbnails/markets/markets-thumb-placeholder-02.png") },
-            { key: 'Market3', title: 'Anthem Farmers Market', address: '41703 N. Gavilan Peak Pkwy Anthem, AZ 85086', image: require("../../assets/thumbnails/markets/markets-thumb-placeholder-03.png") },
-            { key: 'Market4', title: 'Market Names Goes Here', address: '5757 N. Central Ave Phoenix, AZ 85013', image: require("../../assets/thumbnails/markets/markets-thumb-placeholder-04.png") },
-  ]}
-          renderItem={({ item, index }) => <RecordTile item={item} index={index} type="Markets" />}
-        />
-      </View>
+        <View>
+          <FlatList
+              data={[
+                {
+                  key: 'Market1',
+                  title: 'Avondale Care1st Resource',
+                  address: '16838 E Ave of the Fountains Fountain Hills, AZ 85268',
+                  image: require("../../assets/thumbnails/markets/markets-thumb-placeholder-01.png")
+                },
+                {
+                  key: 'Market2',
+                  title: 'Our Harvest/Nuestra Cosec',
+                  address: '3806 N. Brown Rd Scottsdale, AZ 85251',
+                  image: require("../../assets/thumbnails/markets/markets-thumb-placeholder-02.png")
+                },
+                {
+                  key: 'Market3',
+                  title: 'Anthem Farmers Market',
+                  address: '41703 N. Gavilan Peak Pkwy Anthem, AZ 85086',
+                  image: require("../../assets/thumbnails/markets/markets-thumb-placeholder-03.png")
+                },
+                {
+                  key: 'Market4',
+                  title: 'Market Names Goes Here',
+                  address: '5757 N. Central Ave Phoenix, AZ 85013',
+                  image: require("../../assets/thumbnails/markets/markets-thumb-placeholder-04.png")
+                },
+              ]}
+              renderItem={({item, index}) => <RecordTile item={item} index={index} type="Markets"/>}
+          />
+        </View>
     )
   }
 };
