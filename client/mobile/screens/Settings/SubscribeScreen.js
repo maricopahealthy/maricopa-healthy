66d1e0ec01b6a2cb1217e36468f8110a761c52ec
Marcus Ames <marcusames00@gmail.com>
2019-05-30 13:53:20 -0700

    Updated Subscribe Screen

--- client/mobile/screens/Settings/SubscribeScreen.js
+++ client/mobile/screens/Settings/SubscribeScreen.js
@@ -1,6 +1,26 @@
 // todo: SubscribeScreen
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import {View, StyleSheet, SectionList} from 'react-native';
+import Section from "../../utils/SectionsUtility";
+import {Container, Content, Icon, Button, Text} from "native-base";
+import SectionHeader from "../../components/SectionHeader";
+
+const weeklyRecipe = [
+  {
+    id:0,
+    text: "Get new recipe ideas delivered right to your inbox! Sign up today and receive a quick and easy recipe to try every Friday. No more, no less—just one great idea each week, and we won’t use your email for anything else. There are lots of delicious recipes waiting, so sign up today and get cooking!"
+  }
+];
+
+const sections = [
+  Section('Weekly Recipe', weeklyRecipe, ({ item }) => {
+    return (
+      <Text style={{margin: 15}}> {item.text} </Text>
+    );
+  })
+];
+
+const extractKey = ({ id }) => id;
 
 /**
  * Primary Screen for managing User's subscriptions.
@@ -9,18 +29,36 @@ export default class SubscribeScreen extends React.Component {
 
   render() {
     return (
-        <View>
-          <Text>Settings Subscribe Screen</Text>
-          <Button title="Subscribe" onPress={() => {}} />
-        </View>
+      <Container>
+        <Content>
+          <Text style={{marginTop: 15, textAlign: "center", fontSize: 20}}>Catchy headline will go here.</Text>
+          <Text style={{margin: 15}}>
+            Description will go here…sed aliquet nibh a feugiat aliquam. Phasellus eleifend posuere quam vel auctor. Maecenas odio ante, sodales eu rhoncus vitae, lobortis et odio. Phasellus sit amet laoreet odio.
+          </Text>
+          <SectionList
+            sections={sections}
+            renderSectionHeader={SectionHeader}
+            keyExtractor={extractKey}
+          />
+          <View style={styles.container}>
+            <Button style={styles.button} full iconRight>
+              <Text>Subscribe</Text>
+              <Icon name={"ios-lock"}/>
+            </Button>
+          </View>
+        </Content>
+      </Container>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
+    alignSelf: "center"
   },
+  button : {
+    width: "50%",
+    backgroundColor: "#B52126",
+    textAlign: "center"
+  }
 });
