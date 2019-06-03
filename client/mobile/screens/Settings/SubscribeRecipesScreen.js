347bb41bdd4e26720b77d32258bafaeb4a8a6216
Marcus Ames <marcusames00@gmail.com>
2019-06-03 15:11:25 -0700

    added state to recipe subscribe screen

--- client/mobile/screens/Settings/SubscribeRecipesScreen.js
+++ client/mobile/screens/Settings/SubscribeRecipesScreen.js
@@ -42,7 +42,7 @@ export default class SubscribeRecipesScreen extends React.Component {
                 <Label>Email Address</Label>
                 <Input onChangeText={(value) => { this.setState({email: value })}} value={this.state.email} name="email" />
               </Item>
-              <Item stackedLabel last>
+              <Item stackedLabel>
                 <Label>Zip Code</Label>
                 <Input onChangeText={(value) => { this.setState({zip: value })}} value={this.state.zip} name="zip" />
               </Item>
@@ -50,16 +50,16 @@ export default class SubscribeRecipesScreen extends React.Component {
               <Text style={{marginTop: 15, marginLeft: 15, fontSize: 20}}>Confirming your permission: </Text>
               <Text style={styles.container}>Maricopa Healthy believes in transparency and protecting your personal information. The information collected in this form will only be used ti send you healthy recipe ideas once a week and analyze the geographic use of our app (zip code). We will not sell or rent your information to any governmental, commercial or other entity.</Text>
 
-              <View style={styles.container}>
+              <View style={styles.check}>
                 <CheckBox onPress={() => { this.setState({checked: !this.state.checked })}} checked={this.state.checked} color="#B52126"/>
-                <Text>
+                <Text style={{width: "90%"}}>
                   By checking this box, I agree to the use of my personal information as stated above and would like to receive weekly recipe ideas from Maricopa Healthy.
                 </Text>
               </View>
 
               <Text style={styles.container}>You can change your mind at any time by clicking the unsubscribe link in the footer of any email you receive from us, or by contacting use at MaricopaHealthy@mail.maricopa.gov. We will treat your information with respect. For more information about our privacy practices , please visit the Privacy section of our app or https://www.maricopa.gov/4805/Maricopa-Healthy-App-Privacy-Policy. By tapping the red Sign me up! button below, you agree that we may process your information in accordance with these terms.  </Text>
 
-              <Button block style={{backgroundColor: "#B52126"}}>
+              <Button disabled={!this.state.checked} block style={{backgroundColor: "#B52126"}}>
                 <Text>
                   Sign me up!
                 </Text>
@@ -75,4 +75,10 @@ const styles = StyleSheet.create({
   container: {
     margin: 15,
   },
+  check: {
+    width: "100%",
+    flex: 2,
+    flexDirection: "row",
+    justifyContent: "space-between"
+  }
 });
