b91402da69c416aa5dbcf8af110f564c316249e6
Jen <jennifer.nummerdor@gmail.com>
2019-06-06 11:52:47 -0700

    Styling for dashboard is coming along-nav bar is not complete yet, placeholders are being used for analytics data.

--- client/cms/src/components/Dashboard.js
+++ client/cms/src/components/Dashboard.js
@@ -9,6 +9,11 @@ const Dashboard = () => {
             <HeaderNav/>
             <Nav/>
             <div className={"overview-container"}>Overview</div>
+            <div className={"analytics-container"}>
+                <img className="analytics-placeholder" src={"https://maricopa-healthy.s3-us-west-2.amazonaws.com/cms/analytics-placeholder-dashboard.jpg"}/>
+                <img className="analytics-placeholder-apptype" src={"https://maricopa-healthy.s3-us-west-2.amazonaws.com/cms/analytics-placeholder-apptype.jpg"}/>
+                <img className="analytics-placeholder-push" src={"https://maricopa-healthy.s3-us-west-2.amazonaws.com/cms/analytics-placeholder-pushnotifications.jpg"}/>
+            </div>
         < /div>
 
     )
