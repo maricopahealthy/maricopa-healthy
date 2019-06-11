ec4b02b021cbd64e190fb9dcfb233e885e6d19f5
Jen <jennifer.nummerdor@gmail.com>
2019-06-11 09:20:21 -0700

    Fixed the issue of React not updating when editing different markets

--- client/cms/src/components/AddMarket.js
+++ client/cms/src/components/AddMarket.js
@@ -4,26 +4,43 @@ class AddMarket extends React.Component {
     constructor(props){
         super(props)
         this.state = {
-
+            name: '',
+            thumbnail: '',
+            street_address: '',
+            created_at: '',
+            payment_methods: '',
+            phone_number: '',
+            state: '',
+            website: '',
+            zipcode: ''
         }
         this.handleAdd = this.handleAdd.bind(this)
+        this.handleChange = this.handleChange.bind(this)
     }
     handleAdd(){
-        //send get request to retrieve new id
+        this.setState({
+            created_at: new Date()
+
+        })
+
+    }
+    handleChange(e){
+        this.setState({
+            [e.target.name]: e.target.value
+        })
     }
     render(){
         return(<div>
             <form onSubmit={this.handleAdd}>
-                <input type={"text"} name={"name"} placeholder={"Name of Market"}/>
-                <input type={"text"} name={"thumbnail"} placeholder={"Thumbnail Image"}/>
-                <input type={"text"} name={"street_address"} placeholder={"Street Address"}/>
-                <input type={"date"} name={"created_at"}/>
+                <input type={"text"} name={"name"} placeholder={"Name of Market"} onChange={this.handleChange}/>
+                <input type={"text"} name={"thumbnail"} placeholder={"Thumbnail Image"} onChange={this.handleChange}/>
+                <input type={"text"} name={"street_address"} placeholder={"Street Address"} onChange={this.handleChange}/>
 
-                <input type={"text"} name={"payment_methods"} placeholder={"payment_methods"}/>
-                <input type={"tel"} name={"phone_number"} />
-                <input type={"text"} name={"state"} value={"AZ"}/>
-                <input type={"text"} name={"website"} placeholder={"Website URL"}/>
-                <input type={"text"} name={"zipcode"} placeholder={"zipcode"}/>
+                <input type={"text"} name={"payment_methods"} placeholder={"payment_methods"} onChange={this.handleChange}/>
+                <input type={"tel"} name={"phone_number"} onChange={this.handleChange}/>
+                <input type={"text"} name={"state"} value={"AZ"} onChange={this.handleChange}/>
+                <input type={"text"} name={"website"} placeholder={"Website URL"} onChange={this.handleChange}/>
+                <input type={"text"} name={"zipcode"} placeholder={"zipcode"} onChange={this.handleChange}/>
                 <button type={"submit"}>Add Market</button>
             </form>
         </div>)
--- client/cms/src/components/EditMarket.js
+++ client/cms/src/components/EditMarket.js
@@ -4,42 +4,103 @@ class EditMarket extends React.Component {
     constructor(props){
         super(props)
         this.state = {
-            marketPreEdit: [],
-            marketPostEdit: []
+            city: '',
+            created_at: '',
+            hours: [],
+            name: '',
+            payment_methods: '',
+            phone_number: '',
+            rating: '',
+            reviews: '',
+            state: '',
+            street_address: '',
+            thumbnail: '',
+            updated_at: '',
+            website_url: '',
+            zipcode: ''
         }
+        this.fetchMarket = this.fetchMarket.bind(this)
         this.handleEdit = this.handleEdit.bind(this)
         this.handleDelete = this.handleDelete.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+    }
+    fetchMarket(marketId = this.props.match.params.id){
+        fetch(`http://localhost:9000/markets/${marketId}`)
+            .then(res => res.json())
+            .then(markets => {
+                console.log(markets)
+                let market = markets[0]
+                this.setState({
+                    city: market.city,
+                    created_at: market.created_at,
+                    hours: [],
+                    name: market.name,
+                    payment_methods: market.payment_methods,
+                    phone_number: market.phone_number,
+                    rating: market.rating,
+                    reviews: market.reviews,
+                    state: market.state,
+                    street_address: market.street_address,
+                    thumbnail: market.thumbnail,
+                    updated_at: market.updated_at,
+                    website_url: market.website_url,
+                    zipcode: market.zipcode
+                })
+            })
     }
     handleDelete(){
         //run some sort of alert asking for confirmation
         //send delete request to server
+    }
+    handleChange(e){
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+        console.log(this.state, 'state')
     }
     handleEdit(){
         //build out update functionality for server --patch or post?
         fetch('http://localhost:9000/markets/patch')
     }
     componentDidMount(){
-        const marketId = this.props.match.params.id
-        fetch(`http://localhost:9000/markets/${marketId}`)
-            .then(res => res.json())
-            .then(market => this.setState({marketPreEdit: market}))
+        this.fetchMarket()
+    }
+    componentDidUpdate(prevProps) {
+        const currentMarketId = this.props.match.params.id;
+        const prevMarketId = prevProps.match.params.id
+        if(currentMarketId !== prevMarketId){
+            this.fetchMarket(currentMarketId)
+        }
     }
     render(){
-        const market = this.state.marketPreEdit[0] || []
-        {console.log(market)}
         return(<div className={"editMarket-container"}>
+            {/*{this.state.length ? () : null }*/}
             <form onSubmit={this.handleEdit}>
-                <input type={"number"} name={"id"} value={market.id}/>
-                <input type={"text"} name={"name"} value={market.name}/>
-                <input type={"text"} name={"thumbnail"} value={market.thumbnail}/>
-                <input type={"text"} name={"street_address"} value={market.street_address}/>
-                {/*<input type={"date"} name={"created_at"} value={market.created_at}/>*/}
+                Name of Event:
+                <input type={"text"} name={"name"} defaultValue={this.state.name} onChange={this.handleChange}/>
+                Thumbnail Image:
+                <input type={"text"} name={"thumbnail"} defaultValue={this.state.thumbnail} onChange={this.handleChange}/>
+                <img alt="markets-thumbnail" src={this.state.thumbnail}/>
+                Address:
+                <input type={"text"} name={"street_address"} defaultValue={this.state.street_address}/>
+                Created at:
+                <div className={"markets-created-at"}>{this.state.created_at}</div>
+                Last Updated:
+                <div className={"markets-updated-at"}>{this.state.updated_at}</div>
+                {/*{this.state.hours.map(hour => {*/}
+                {/*    {<input type={"text"} name="hours" value={hour}/>}*/}
+                {/*})}*/}
+                Payment Methods:
+                <input type={"text"} name={"payment_methods"} defaultValue={this.state.payment_methods}/>
+                Contact Phone:
+                <input type={"tel"} name={"phone_number"} defaultValue={this.state.phone_number}/>
+                State:
+                <input type={"text"} name={"state"} defaultValue={this.state.state}/>
+                Website Url:
+                <input type={"text"} name={"website"} defaultValue={this.state.website_url}/>
+                Zip Code:
+                <input type={"text"} name={"zipcode"} defaultValue={this.state.zipcode}/>
 
-                <input type={"text"} name={"payment_methods"} value={market.payment_methods}/>
-                <input type={"tel"} name={"phone_number"} value={market.phone_number}/>
-                <input type={"text"} name={"state"} value={market.state}/>
-                <input type={"text"} name={"website"} value={market.website_url}/>
-                <input type={"text"} name={"zipcode"} value={market.zipcode}/>
                 <button type={"delete"} onClick={this.handleDelete}>Delete</button>
                 <button type={"submit"}>Update</button>
             </form>
